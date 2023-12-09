52461d6f9052652dfa231214d6e566fbe97f994f
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-12-09 09:09:05 +0000

    CHANGES SO FAR

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
 #!/home/python/venv/main/bin/python
 
-def main():
-  return "Hey there"
+from dotenv import load_dotenv
+from os import environ
+from web3 import Web3
+
+load_dotenv(".env")
+
+INFURA_API_KEY = environ.get("INFURA_API_KEY")
+INFURA_SEPOLIA_URL = f"https://sepolia.infura.io/v3/${INFURA_API_KEY}"
+
+
+def main() -> str    :
+    Web3.HTTPProvider()
 
 if __name__ == "__main__":
   print(main())
